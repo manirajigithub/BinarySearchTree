f4d136025eb07681cbab452d51e1a4c35705c5ec
mani1 <mani1@NANI143>
2022-08-14 18:19:28 +0530

    [Rajeswari]Added Program.cs

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿namespace BinarySearchTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Binary Search Tree");
+            BinarySearchTree<int> binarySearch = new BinarySearchTree<int>(56);
+            binarySearch.Insert(30);
+            binarySearch.Insert(70);
+            Console.WriteLine("InOrder Traversal For Given Is BST ");
+            binarySearch.Display();
+            Console.ReadLine();
+        }
+    }
+}
